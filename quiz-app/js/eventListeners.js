75e6acaf74bcc4b9ad2d42ee2fe3b6863d172d57
jpke <j.patrick.earnest@gmail.com>
2016-09-20 01:31:17 -0400

    3.2.4 responses integrated into page.

--- quiz-app/js/eventListeners.js
+++ quiz-app/js/eventListeners.js
@@ -1,34 +1,3 @@
-function renderResponse(state, response) {
-  if(response) {
-    return alert("Correct. Click 'Next' to advance.");
-  } else {
-    return alert("Incorrect. The correct answer is: " + state.questions[state.index][1]);
-  }
-};
-
-function renderProgress(state, element) {
-  var progress = "<span>Question: " + (state.index) + " of 10</span>";
-  element.html(progress);
-};
-
-function renderScore(state, element) {
-  var score = state.record.reduce((a, b) => a + b, 0);
-  if(score == 1) {
-    var scoreHTML ="<span>Score: " + score + " question correct</span>";
-  } else {
-    var scoreHTML = "<span>Score: " + score + " questions correct</span>";
-  }
-  element.html(scoreHTML);
-};
-
-function renderQuiz(state) {
-  renderQuestion(state, $('.quiz-question'));
-  renderAnswers(state, $('.quiz-answers'));
-  randomizeAnswers();
-  renderProgress(state, $('.progress'));
-  renderScore(state, $('.score'));
-}
-
 $('#start').on('click', function(event) {
   event.preventDefault();
   state.index = 1;
@@ -43,9 +12,9 @@ $('#start').on('click', function(event) {
 $('#next').on('click', function(event) {
   event.preventDefault();
   if($('.quiz-answers').get(0).classList.contains('unclicked')) {
-    return alert("You must make a selection before continuing.")
+    return $('.response').html("You must make a selection before continuing. Select from the options below");
   };
-  
+
   if(state.index < 10) {
     state.index++;
     renderQuiz(state);
@@ -53,6 +22,7 @@ $('#next').on('click', function(event) {
   } else {
     $('.quiz').addClass('hidden');
     $('.finished').removeClass('hidden');
+    $('.totalScore').html(state.record.reduce((a, b) => a + b, 0));
     $('#start').removeClass('hidden');
     $('#next').addClass('hidden');
   }
@@ -60,12 +30,14 @@ $('#next').on('click', function(event) {
 
 $('.quiz-answers').on('click', 'li', function(event) {
   event.preventDefault();
+  //check if first try on question
   if($('.quiz-answers').get(0).classList.contains('unclicked')) {
     $('.quiz-answers').removeClass('unclicked');
     var response = evaluateResponse($(this).attr('class'));
     renderResponse(state, response);
     renderScore(state, $('.score'));
   } else {
-      alert("Selection already made. Click next to advance.");
+      // alert("Selection already made. Click next to advance.");
+      $('.response').html("Selection already made. Click next to advance.")
     }
 });
--- quiz-app/js/index.js
+++ quiz-app/js/index.js
@@ -57,43 +57,4 @@ var state = {
   index: 1
 };
 
-var recordResponse = function(state, score) {
-  state.record.push(score);
-};
-
-var renderQuestion = function(state, element) {
-  var questionHTML = '<li>' + state.questions[state.index][0] + '</li>';
-  element.html(questionHTML);
-}
-
-var renderAnswers = function(state, element) {
-  var answersHTML = state.questions[state.index].slice(1,5).map(function(answer) {
-    return '<li>' + answer + '</li>';
-  });
-  // identify correct answer
-  answersHTML[0] = answersHTML[0].replace(/<.*?>/, "<li class='correct'>");
-  element.html(answersHTML);
-};
-
-function randomizeAnswers() {
-  var ul = document.querySelector('.quiz-answers');
-  for (var i = ul.children.length; i >= 0; i--) {
-    ul.appendChild(ul.children[Math.random() * i | 0]);
-  }
-};
-
-function evaluateResponse(responseClass) {
-  if(responseClass == "correct") {
-    state.record.push(1);
-    return true;
-  } else {
-    state.record.push(0);
-    return false;
-  }
-}
-
-
-
-
-
 
