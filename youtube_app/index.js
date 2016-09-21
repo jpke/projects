3f22fb8876dd1e2d804cd1a5c5df253e58a982ac
jpke <j.patrick.earnest@gmail.com>
2016-09-21 11:37:11 -0400

    basic functionality created

---
+++ youtube_app/index.js
@@ -0,0 +1,47 @@
+// Youtube api key: AIzaSyDKUr7nLe1fsOaQt-1CtrPlujbMv4EWBEo
+
+var youtube_base_url = 'https://www.googleapis.com/youtube/v3/search'
+var data ={};
+
+function getDataFromApi(searchTerm, callback) {
+  console.log("getDataFromApi called");
+  var settings = {
+    url: youtube_base_url,
+    data: {
+      q: searchTerm,
+      r: 'json',
+      part: 'snippet',
+      key: 'AIzaSyDKUr7nLe1fsOaQt-1CtrPlujbMv4EWBEo',
+      maxResults: 10,
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+  data = $.ajax(settings);
+  $.ajax(settings);
+};
+
+function displayYoutubeSearchData(data) {
+  var resultElement = '';
+  if(data.items) {
+    data.items.forEach(function(item) {
+      resultElement += '<li><img src="' + item.snippet.thumbnails.default.url + '"></li>';
+    });
+  }
+  else {
+    resultElement += '<li>No results</li>';
+  }
+
+  $('.js-results').html(resultElement);
+};
+
+function watchSubmit() {
+  $('.js-youtube-query').submit(function(e) {
+    e.preventDefault();
+    var query = $(this).find('.search-query').val();
+    getDataFromApi(query, displayYoutubeSearchData);
+  });
+};
+
+$(function(){watchSubmit();});
