298d9d400f9b1deb0cf2770f2ce18b02de03e2e4
jpke <j.patrick.earnest@gmail.com>
2016-09-21 11:48:40 -0400

    images clickable

--- youtube_app/index.js
+++ youtube_app/index.js
@@ -26,7 +26,7 @@ function displayYoutubeSearchData(data) {
   var resultElement = '';
   if(data.items) {
     data.items.forEach(function(item) {
-      resultElement += '<li><img src="' + item.snippet.thumbnails.default.url + '"></li>';
+      resultElement += '<li><a href="https://www.youtube.com/watch?v=' + item.id.videoId + '"><img src="' + item.snippet.thumbnails.default.url + '"></li>';
     });
   }
   else {
