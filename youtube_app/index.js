470fcf15c7d613114c91f6cd7d310a96e901c5a6
jpke <j.patrick.earnest@gmail.com>
2016-09-21 15:24:14 -0400

    all features except lightbox working

--- youtube_app/index.js
+++ youtube_app/index.js
@@ -1,74 +1,77 @@
 // Youtube api key: AIzaSyDKUr7nLe1fsOaQt-1CtrPlujbMv4EWBEo
+var state = {
+  youtube_base_url: 'https://www.googleapis.com/youtube/v3/search',
+  query: "",
+  data: {},
+  index: 0,
+};
 
-var youtube_base_url = 'https://www.googleapis.com/youtube/v3/search'
-var data ={};
-
-function getDataFromApi(searchTerm, callback) {
+function getDataFromApi(state, callback) {
   var settings = {
-    url: youtube_base_url,
-    data: {
-      q: searchTerm,
+    url: state.youtube_base_url,
+     data: {
+      q: state.query,
       r: 'json',
       part: 'snippet',
       key: 'AIzaSyDKUr7nLe1fsOaQt-1CtrPlujbMv4EWBEo',
-      maxResults: 10,
-    },
-    dataType: 'json',
-    type: 'GET',
-    success: callback
+      maxResults: 6,
+     },
+     dataType: 'json',
+     type: 'GET',
+     success: callback
   };
-  data = $.ajax(settings);
-  $.ajax(settings);
+  
+  state.data = $.ajax(settings);
 };
+
+function getMoreDataFromApi(state, callback) {
+  var settings = {
+    url: state.youtube_base_url,
+     data: {
+       q: state.query,
+       pageToken: state.data.responseJSON.nextPageToken,
+       r: 'json',
+       part: 'snippet',
+       key: 'AIzaSyDKUr7nLe1fsOaQt-1CtrPlujbMv4EWBEo',
+       maxResults: 6,
+    },
+     dataType: 'json',
+     type: 'GET',
+     success: callback
+   };
+  state.data = $.ajax(settings);
+}
+
 
 function displayYoutubeSearchData(data) {
   var resultElement = '';
   if(data.items) {
     data.items.forEach(function(item) {
-      resultElement += '<li><a class="fancybox" rel="group" href="https://www.youtube.com/watch?v=' + item.id.videoId + '"><img src="' + item.snippet.thumbnails.default.url + '"/></a></li>';
+      resultElement += '<li><a class="fancybox" rel="group" href="https://www.youtube.com/watch?v=' + item.id.videoId + '"><img src="' + item.snippet.thumbnails.default.url + '"/></a> <a class="more" href="https://www.youtube.com/channel/' + item.snippet.channelId + '">More from this channel</a></li>';
     });
   }
   else {
     resultElement += '<li>No results</li>';
   }
-
+  // add more results button at end of results list
+  resultElement += "<button type='submit' id='more'>More Results</button>"
   $('.js-results').html(resultElement);
 };
 
-function watchSubmit() {
+function watchSubmit(state) {
   $('.js-youtube-query').submit(function(e) {
     e.preventDefault();
-    var query = $(this).find('.search-query').val();
-    getDataFromApi(query, displayYoutubeSearchData);
-    $(".fancybox").fancybox();
+    state.query = $(this).find('.search-query').val();
+    getDataFromApi(state, displayYoutubeSearchData);
+    // $(".fancybox").fancybox();
+    state.index++;
   });
 };
 
-$(function(){watchSubmit();});
+$(function(){watchSubmit(state);});
 
-// $(document).ready(function() {
-//   $(".fancybox").fancybox();
-// });
-
-// function onYouTubePlayerAPIReady() {
-//     $(document).ready(function () {
-//         $.fancybox({
-//             href: "http://www.youtube.com/embed/L9szn1QQfas?enablejsapi=1&wmode=opaque&autoplay=1",
-//             type: "iframe",
-//             beforeShow: function () {
-//                 // Find the iframe ID
-//                 var id = $.fancybox.inner.find('iframe').attr('id');
-//                 // Create video player object and add event listeners
-//                 var player = new YT.Player(id, {
-//                     events: {
-//                         'onStateChange': function (event) {
-//                             if (event.data === 0) {
-//                                 $.fancybox.close();
-//                             } // if
-//                         } // onStateChange
-//                     } // events
-//                 }); // YT.Player
-//             } // beforeShow
-//         }); // fancybox
-//     }); // ready
-// } // onYouTubePlayerAPIReady
+$('.js-results').on('click', '#more', function(e) {
+  e.preventDefault();
+  getMoreDataFromApi(state, displayYoutubeSearchData);
+  state.index++;
+});
